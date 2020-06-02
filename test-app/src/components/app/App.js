d270a59a6ef526f8e3d8d0f07094314da27867d1
Tema Tema <tematema@MacBook-Pro-Tema.local>
2020-06-02 18:57:35 +0200

    add content

--- test-app/src/components/app/App.js
+++ test-app/src/components/app/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import Header from '../Header/Header';
+import RandomPlanet from '../RandomPlanet/RandomPlanet';
+import ItemList from '../ItemList/ItemList';
+import PersonDetails from '../PersonDetails/PersonDetails';
 
 const App = () => {
   return (
     <div>
-    <h1>Hello World</h1>
+      <Header />
+      <RandomPlanet />
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
     </div>
   );
 };
